4052168f6f998eed55abe902258620a1dac405bb
austenYang <austenyad@gmail.com>
2026-02-10 16:57:02 +0800

    demo commit

--- app/src/main/java/com/yad/godotservicedemo/AppPlugin.kt
+++ app/src/main/java/com/yad/godotservicedemo/AppPlugin.kt
@@ -1,16 +1,28 @@
 package com.yad.godotservicedemo
 
-import android.util.Log
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
-import org.godotengine.godot.plugin.UsedByGodot
+import org.godotengine.godot.plugin.SignalInfo
 
+/**
+ * Runtime [GodotPlugin] used to enable interaction with the Godot gdscript logic.
+ */
 class AppPlugin(godot: Godot) : GodotPlugin(godot) {
+
+    companion object {
+        val SHOW_GLTF_SIGNAL = SignalInfo("show_gltf", String::class.java)
+    }
 
     override fun getPluginName() = "AppPlugin"
 
-    @UsedByGodot
-    fun showToast(message: String) {
-        Log.e("AppPlugin", "-----> $message")
+    override fun getPluginSignals() = setOf(SHOW_GLTF_SIGNAL)
+
+    /**
+     * Used to emit a signal to the gdscript logic to update the gltf being shown.
+     *
+     * @param glbFilepath Filepath of the gltf asset to be shown
+     */
+    internal fun showGLTF(glbFilepath: String) {
+        emitSignal(SHOW_GLTF_SIGNAL.name, glbFilepath)
     }
 }
